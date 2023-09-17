49fb1fb59f31fa8403ecdfd3453d86b2b2e1be04
VitorGreff <vitorgreffdias@gmail.com>
2023-09-17 13:43:16 -0300

    not found exception working

--- src/main/java/com/greff/starwars/Services/PlanetService.java
+++ src/main/java/com/greff/starwars/Services/PlanetService.java
@@ -1,12 +1,14 @@
 package com.greff.starwars.Services;
 
-import com.greff.starwars.Domain.Planet;
 import com.greff.starwars.DTO.PlanetDTO;
+import com.greff.starwars.Domain.Planet;
 import com.greff.starwars.Repositories.PlanetRepository;
+import com.greff.starwars.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PlanetService {
@@ -20,7 +22,8 @@ public class PlanetService {
         return repo.findByName(name);
     }
     public Planet findById(String id){
-        return repo.findById(id).orElseThrow();
+        Optional<Planet> planet = repo.findById(id);
+        return planet.orElseThrow(()-> new ObjectNotFoundException("Object not found in the database!"));
     }
     public Planet insert(PlanetDTO dto){
         return repo.save(dto.fromDTO());
